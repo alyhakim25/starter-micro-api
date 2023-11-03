016bc3c1cd5647cb1a397872064ef9062ad41571
alyhakim25 <38533241+alyhakim25@users.noreply.github.com>
2023-11-03 11:02:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+/***
 var http = require('http');
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
     res.write('Yo!');
     res.end();
 }).listen(process.env.PORT || 3000);
+***/
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Define a simple route
+app.get('/', (req, res) => {
+  res.send('Hello, this is a simple Node.js API!');
+});
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
